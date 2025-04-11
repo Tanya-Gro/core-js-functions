078216c1ca137f51a98b376acf615e3153b9e613
unknown <tarpel.gta@gmail.com>
2025-04-11 14:54:18 +0300

    add: solution 4 task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -70,8 +70,10 @@ function getArgumentsCount(array) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return function pow(x) {
+    return x ** exponent;
+  };
 }
 
 /**
