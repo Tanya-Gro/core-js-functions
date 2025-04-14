54e7dd1e2935ce61c87563f4dd91635805ac5eb1
unknown <tarpel.gta@gmail.com>
2025-04-14 10:46:16 +0300

    add: solution 10 task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -206,8 +206,11 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function result(...args2) {
+    const args = args1.concat(args2);
+    return fn(...args);
+  };
 }
 
 /**
